b18835bbdb11f0fd433dd5e26662edc73501032f
arkarsoe <arkarsoedev@gmail.com>
2024-11-17 02:46:08 +0630

    update

--- middleware.ts
+++ middleware.ts
@@ -7,14 +7,14 @@ const handleI18nRouting = createMiddleware(routing);
 
 const customMiddleware = async (request: NextRequest) => {
   const ip = request.headers.get('x-forwarded-for') || 'unknown'
-  let userCountry = await getUserCountry(ip) || 'en'
-  let localeCode = userCountry.countryCode.toLowerCase()
+
   const storedLocale = (request.cookies.get('NEXT_LOCALE')?.value || 'false') === 'true'
   // response.headers.set('x-user-ip', ip)
 
-  const [, locale, ...segments] = request.nextUrl.pathname.split('/');
-
   if (!storedLocale) {
+    let userCountry = await getUserCountry(ip)
+    let localeCode = userCountry.countryCode.toLowerCase()
+    const [, locale, ...segments] = request.nextUrl.pathname.split('/');
     request.nextUrl.pathname = `/${localeCode}/${segments.join('/')}`;
     request.cookies.set('NEXT_LOCALE', localeCode)
   }
