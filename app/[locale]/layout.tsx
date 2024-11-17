cddbf1ea71e9fb72f80ac66bdb7f9b7a89a9244d
arkarsoe <arkarsoedev@gmail.com>
2024-11-17 22:58:20 +0630

    update

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,9 +2,9 @@ import { NextIntlClientProvider } from 'next-intl';
 import { getMessages, setRequestLocale } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
-import Options from '@/componnets/options';
-import { cookies, headers } from 'next/headers';
-import { getUserCountry } from '@/actions/getUserCountry';
+import Options from '@/components/options';
+import { headers } from 'next/headers';
+import { ModalProvider } from '@/providers/modal-provider'
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
@@ -18,24 +18,11 @@ export default async function LocaleLayout({
   params: Promise<{ locale: string }>;
 }) {
   const { locale } = await params
-  const headerList = await headers()
-  let userLocale = 'en'
 
   // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale as "en" | "mm")) {
     notFound();
   }
-
-
-  // if (locale) {
-  //   userLocale = locale
-  // } else if (headerList.get('x-user-ip')) {
-  //   console.log(headerList.get('x-user-ip'))
-  //   let userIp = headerList.get('x-user-ip') || 'en'
-
-  //   let userCountry = await getUserCountry(userIp) || 'en'
-  //   userLocale = userCountry.countryCode.toLowerCase()
-  // }
 
   // Enable static rendering
   setRequestLocale(locale);
@@ -49,6 +36,7 @@ export default async function LocaleLayout({
       <body>
         <NextIntlClientProvider messages={messages}>
           <Options locale={locale} />
+          <ModalProvider />
           {children}
         </NextIntlClientProvider>
       </body>
